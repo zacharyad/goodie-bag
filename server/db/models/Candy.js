ba4c4e09770397d60a162c49c7db3e21f8773ac3
zacharyAD <zacharyadcoding@gmail.com>
2019-02-08 15:27:09 -0500

    got the db model running and seeded

--- server/db/models/Candy.js
+++ server/db/models/Candy.js
@@ -1,6 +1,31 @@
 const Sequelize = require('sequelize');
 const db = require('../database');
 
-module.exports = db.define('candy', {
-  // define your model here!
+const Candy = db.define('candy', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      max: 10,
+    },
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    default:
+      'https://banner2.kisspng.com/20180127/cdw/kisspng-candy-confectionery-illustration-candy-logo-5a6c67a4ef7473.4801257015170538609808.jpg',
+  },
 });
+
+module.exports = Candy;
